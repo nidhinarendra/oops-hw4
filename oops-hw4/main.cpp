29996c7d753a922a2b9a42a0cab6d5a647d3dfb0
stupidfellow <nidhi.narendra1@gmail.com>
2016-04-25 13:39:00 -0700

    Added another function to validate the boundary. But global variable not working

--- oops-hw4/main.cpp
+++ oops-hw4/main.cpp
@@ -3,14 +3,37 @@
 #include<iostream>
 using namespace std;
 
-// example matrix size
+// example matrix size declared globally
 const int row (8);
 const int col (8);
+
+//Function to validate the matrix
+void Validate(int matrix[][col], int r, int c)
+{
+    if (r==0 || r == row-1 || c==0 || c== col-1)
+    {
+    cerr << "There is no boundary and hence the cannot be filled" << endl;
+    exit(0);
+    }
+    
+    else if (matrix[r][c] == 1 || matrix[r][c] == -1)
+    {
+        return;
+    }
+    matrix[r][c] = -1;
+    
+    Validate(matrix, r-1, c);
+    Validate(matrix, r-1, c);
+    Validate(matrix, r, c+1);
+    Validate(matrix, r, c-1);
+    
+}
 
 //Function to fill the neighbours
 void FillNeig(int matrix[][col], int r, int c, int tofill)
 {
-    if(matrix[r][c] != 0) //base case
+    
+    if(matrix[r][c] != -1) //base case
       return;
     
     matrix[r][c] = tofill; //change the color of matrix at index [x][y].
@@ -39,16 +62,17 @@ void printmatrix( int matrix[][col], int row, int col)
 int main()
 {
     int matrix[][col] = {{0, 0, 0, 0, 0, 0, 0, 0},
-                         {0, 1, 1, 1, 1, 1, 0, 0},
-                         {0, 1, 0, 0, 0, 1, 0, 0},
-                         {0, 1, 0, 0, 0, 1, 0, 0},
-                         {0, 1, 1, 1, 1, 0, 0, 0},
+                         {0, 1, 0, 0, 0, 0, 0, 0},
+                         {1, 0, 1, 0, 0, 0, 0, 0},
+                         {0, 1, 0, 0, 0, 0, 0, 0},
+                         {0, 0, 0, 0, 0, 0, 0, 0},
                          {0, 0, 0, 0, 0, 0, 0, 0},
                          {0, 0, 0, 0, 0, 0, 0, 0},
                          {0, 0, 0, 0, 0, 0, 0, 0},
     };
     
-    int r = 3, c = 3, tofill = 2; //Example input where the color needs to be filled
+    int r = 2, c = 1, tofill = 1; //Example input where the color needs to be filled
+    Validate(matrix, r, c);
     FillNeig(matrix, r, c, tofill);
     printmatrix(matrix, row, col);
 }
